32a204a917c0df2dc2668d4f8d989bc5269cf22e
Karson Lant <lantk991@gmail.com>
2026-01-05 20:29:31 -0800

    sol to remove elements

---
+++ arrays/RemoveElement.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[l] = nums[i]
+                l += 1
+        
+        return l
+    
+nums = [1, 2, 3, 4, 5]
+sol = Solution()
+
+print(sol.removeElement(nums, 2))
+print(nums)
